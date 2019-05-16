0b9a402c5cb23ae59074f6324dfbdb4b16ea27d9
Seojiyoon <49122759+Seojiyoon@users.noreply.github.com>
2019-05-16 18:24:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     name="test",
     version="0.1",
