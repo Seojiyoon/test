ad6d838dd4509fd92b5a065cf6d5744e2892286d
jseo <sjy950462@gmail.com>
2019-05-16 13:53:51 +0900

    test

---
+++ package.py
@@ -0,0 +1,10 @@
+def my_test():
+
+    for x in range(2, 10):
+        print("------------------")
+        for y in range(1, 10):
+            print(x, "X", y, "=", x * y)
+    print("---------------------")
+
+
+print(my_test())
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test",
+    version="0.1",
+    description="",
+    author="jseo",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="キーワード",
+    url="https://github.com/Seojiyoon/test",
+    packages=find_packages(),
+
+)
